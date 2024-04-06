b7d81457e6ca168e5c026f001294126b20459acc
Madhavi0023 <145922096+Madhavi0023@users.noreply.github.com>
2024-04-06 18:24:23 +0530

    Delete server directory

--- server/mongoose.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-const connectDB = async () => {
-  await mongoose.connect(process.env.DB_URL);
-  console.log("Connected to mongoDB database using mongoose");
-};
-
-export default connectDB;
